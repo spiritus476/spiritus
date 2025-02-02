201da5f98982720e6a45d73e438df194e33e86a6
spiritus476 <spiritus476@gmail.com>
2025-02-02 18:30:17 +0530

    Update AppContext.jsx

--- spiritus/src/context/AppContext.jsx
+++ spiritus/src/context/AppContext.jsx
@@ -5,7 +5,7 @@ export const AppContext= createContext()
 
 const AppContextProvider=(props)=>{
     const currencySymbol='₹'
-    const backendUrl = 'https://spiritus-server.onrender.com'
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
 
     const [doctors,setDoctors] = useState([])
     const [token,setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : false)
